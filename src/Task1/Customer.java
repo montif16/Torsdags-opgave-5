469c9dd7f4d6051a472adfe916e1b522a52f3a39
montif16 <Lukas1532@hotmail.com>
2024-03-06 21:53:55 +0100

    Task one complete.
    Added all tasks through 1.a --> 1.f

--- src/Task1/Customer.java
+++ src/Task1/Customer.java
@@ -1,4 +1,35 @@
 package Task1;
 
 public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private int id;
+    private static int counter;
+
+    public Customer(String firstName, String lastName, String username){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        counter += 1;
+        this.id = counter;
+    }
+
+    @Override
+    public String toString(){
+        return "First name is: "+this.firstName+"\nLast name is: "+this.lastName+"\nUsename is: "+username+"\n";
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public int getId() {
+        return id;
+    }
 }
--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,7 +1,32 @@
 package Task1;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Creates new ArrayList object called "customers"
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        //Creating object "john" and using contructor to set: First name, Last name and username.
+        customers.add(new Customer("John","Hansen","80085"));
+        customers.add(new Customer("Peter","Mattisen","SausageOligarch"));
+
+        //Below is to test if counter goes up when a Customer object is created.
+        //System.out.println(Customer.counter);
+
+        //Prints to result of using our custom toString() method on our object "john"
+        //Outdated.
+        //System.out.println(john.toString());
+
+        //Using our printCustomers method. It takes an ArrayList of type Customer.
+        //Prints out all customers in our ArrayList
+        printCustomers(customers);
+    }
+    public static void printCustomers(ArrayList<Customer> customers){
+
+        //For each customer in our Customer class, print out each customer using .toString.
+        for (Customer customer: customers){
+            System.out.println(customer.toString());
+
+        }
     }
 }
